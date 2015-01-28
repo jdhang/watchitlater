675ce95e9425362ff43384e2559cfa1f4128bfdb
Jason Hang <jason.d.hang@gmail.com>
2015-01-27 23:35:10 -0500

    Added sessions controller and all of its views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+  before_action :require_login, only: [:dashboard, :watched_movies]
+
+  def new
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password]
+    if user = User.authenticate(username, password)
+      session[:user_id] = user.id
+      redirect_to dashboard_path, notice: "You have successfully logged in."
+    else
+      redirect_to login_path, error: "Your username or password was incorrect."
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "You have successfully logged out."
+  end
+
+  def dashboard
+    @unwatched_movies = current_user.unwatched_movies
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def watched_movies
+    @watched_movies = current_user.watched_movies
+    respond_to do |format|
+      format.js
+    end
+  end
+
+end
